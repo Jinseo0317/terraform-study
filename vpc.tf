db2cc0adb566d4a30d0b95b56f20a3227f8edd7c
jinseo <ghkdwlstj99@gmail.com>
2023-07-23 18:59:35 +0900

    feat: igw 및 rt 구성 추가

--- vpc.tf
+++ vpc.tf
@@ -22,13 +22,39 @@ resource "aws_subnet" "terraform_sub1"{
     }
 }
 
-resource "aws_subnet" "terraform-sub2"{
+resource "aws_subnet" "terraform_sub2"{
     vpc_id = aws_vpc.main.id
     cidr_block = "10.0.2.0/24"
 
     availability_zone = "ap-northeast-2"
 
     tags = {
-        Name = "terraform_sub1"
+        Name = "terraform_sub2"
     }
+}
+
+resource "aws_internet_gateway" "main_igw" {
+    vpc_id = aws_vpc.main.id
+
+    tags = {
+        Name = "main-igw"
+    }
+}
+
+resource "aws_route_table" "main_route_table"{
+    vpc_id = aws_vpc.main.id
+
+    tags = {
+        Name = "main-rt"
+    }
+}
+
+resource "aws_route_table_association" "route_table_association_1"{
+    subnet_id = aws_subnet.terraform_sub1.id
+    route_table_id = aws_route_table.main_route_table.id
+}
+
+resource "aws_route_table_association" "route_table_association_2"{
+    subnet_id = aws_subnet.terraform_sub2.id
+    route_table_id = aws_route_table.main_route_table.id
 }
