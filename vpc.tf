7d1c216e8cdd113d9aa707fdec9fb52d9f6a86d3
jinseo <ghkdwlstj99@gmail.com>
2023-07-23 22:16:48 +0900

    feat: eip 및 nat gateway 구성 코드 작성

--- vpc.tf
+++ vpc.tf
@@ -33,6 +33,8 @@ resource "aws_subnet" "terraform_sub2"{
     }
 }
 
+// igw 구성
+
 resource "aws_internet_gateway" "main_igw" {
     vpc_id = aws_vpc.main.id
 
@@ -40,6 +42,8 @@ resource "aws_internet_gateway" "main_igw" {
         Name = "main-igw"
     }
 }
+
+// Route Table 구성
 
 resource "aws_route_table" "main_route_table"{
     vpc_id = aws_vpc.main.id
@@ -58,6 +62,8 @@ resource "aws_route_table_association" "route_table_association_2"{
     subnet_id = aws_subnet.terraform_sub2.id
     route_table_id = aws_route_table.main_route_table.id
 }
+
+// Private Subnet 구성
 
 resource "aws_subnet" "terraform_private_sub1"{
     vpc_id = aws_vpc.main.id
@@ -78,5 +84,45 @@ resource "aws_subnet" "terraform_private_sub2"{
 
     tags = {
         Name = "terraform-private-sub2"
+    }
+}
+
+// private subnet 연결을 위한 eip 구성
+
+resource "aws_eip" "nat1" {
+    vpc = true
+
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+resource "aws_eip" "nat2" {
+    vpc = true
+
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+// nat gateway 구성
+
+resource "aws_nat_gateway" "nat_gateway_1"{
+    allocation_id = aws_eip.nat1.id
+
+    subnet_id = aws_subnet.terraform_sub1.id
+
+    tags = {
+        Name = "nat-gateway-1"
+    }
+}
+
+resource "aws_nat_gateway" "nat_gateway_2"{
+    allocation_id = aws_eip.nat2.id
+
+    subnet_id = aws_subnet.terraform_sub2.id
+
+    tags = {
+        Name = "nat-gateway-2"
     }
 }
