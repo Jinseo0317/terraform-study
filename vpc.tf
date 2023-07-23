e07c6e944c6e7ab2c6604e51485b48a50eb43e7c
jinseo <ghkdwlstj99@gmail.com>
2023-07-23 22:00:18 +0900

    feat: 프라이빗 서브넷 구성 코드 작성

--- vpc.tf
+++ vpc.tf
@@ -58,3 +58,25 @@ resource "aws_route_table_association" "route_table_association_2"{
     subnet_id = aws_subnet.terraform_sub2.id
     route_table_id = aws_route_table.main_route_table.id
 }
+
+resource "aws_subnet" "terraform_private_sub1"{
+    vpc_id = aws_vpc.main.id
+    cidr_block = "1.0.3.0/24"
+
+    availability_zone = "ap-northeast-2a"
+
+    tags = {
+        Name = "terraform-private-sub1"
+    }
+}
+
+resource "aws_subnet" "terraform_private_sub2"{
+    vpc_id = aws_vpc.main.id
+    cidr_block = "1.0.4.0/24"
+
+    availability_zone = "ap-northeast-2b"
+
+    tags = {
+        Name = "terraform-private-sub2"
+    }
+}
